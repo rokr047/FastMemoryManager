be1c6c0b8558eda3cd01255b0f1f9a434f16f7cb
Karthikeyan Sreenivasan <rokr047@gmail.com>
2014-04-25 03:33:02 -0700

    Implemented all functions, but there is a bug in either remove or add

--- FMManager.h
+++ FMManager.h
@@ -2,22 +2,40 @@
 #include <new>
 #include <assert.h>
 
-#define DEBUG 0
-
 using namespace std;
+
+typedef struct tag_Header
+{
+  char* bufferStart;
+  unsigned bufferSize;
+  void* head;
+  unsigned numOfFreeBlocks;
+  unsigned numOfAllocatedBlocks;
+}Header;
+
+typedef struct tag_Bookmark
+{
+    void* nextEmptyBlock;
+}Bookmark;
 
 template<typename T>
 class FMManager
 {
 private:
-    char* bufferStart;
-
+    Header* m_head;
 public:
     FMManager();
-    FMManager(int bufferSize);
+    FMManager(unsigned bufferSize);
     ~FMManager();
 
     char* GetBufferStart();
+    T *	Add();
+    void Remove(T *);
+    unsigned Count();
+	unsigned Capacity();
+
+	bool IsEmpty();
+	bool IsFull();
 };
 
 /** \brief
@@ -27,7 +45,8 @@ public:
 template<typename T>
 FMManager<T>::FMManager()
 {
-    bufferStart =  NULL;
+    m_head->bufferStart =  NULL;
+    m_head->head = NULL;
     cout << "\n\nERROR : Using the incorrect FMManager Constructor!!!\n";
 }
 
@@ -38,14 +57,107 @@ FMManager<T>::FMManager()
  *  bufferSize - The size of the buffer that needs to be allocated.
  */
 template<typename T>
-FMManager<T>::FMManager(int bufferSize)
+FMManager<T>::FMManager(unsigned bufferSize)
 {
-    bufferStart = (char*)malloc(sizeof(bufferSize * sizeof(char)));
-    assert(bufferStart != NULL);
+    assert(sizeof(Bookmark) <= sizeof(T));
+
+    m_head = (Header*)malloc(sizeof(Header));
+    assert(m_head != NULL);
+
+    m_head->bufferSize = bufferSize;
+    m_head->bufferStart = (char*)malloc(sizeof(bufferSize * sizeof(char)));
+    assert(m_head->bufferStart != NULL);
+
+    m_head->numOfAllocatedBlocks = 0;
+    m_head->numOfFreeBlocks = 0 ;
+    m_head->head = m_head->bufferStart;
 }
+
+template<typename T>
+FMManager<T>::~FMManager()
+{
+    m_head->bufferStart = NULL;
+    delete m_head->bufferStart;
+
+    m_head->head = NULL;
+    delete m_head->head;
+
+    m_head = NULL;
+    delete m_head;
+}
+
+/** \brief
+ *  Returns the start pointer of the buffer as char pointer.
+ * \return
+ *  char*
+ */
 
 template<typename T>
 char* FMManager<T>::GetBufferStart()
 {
-    return bufferStart;
+    return m_head->bufferStart;
+}
+
+template<typename T>
+unsigned FMManager<T>::Count()
+{
+    return m_head->numOfAllocatedBlocks;
+}
+
+template<typename T>
+unsigned FMManager<T>::Capacity()
+{
+    return m_head->bufferSize/sizeof(T) ;
+}
+
+template<typename T>
+bool FMManager<T>::IsEmpty()
+{
+    return (m_head->numOfAllocatedBlocks == 0);
+}
+
+template<typename T>
+bool FMManager<T>::IsFull()
+{
+    return (m_head->numOfAllocatedBlocks == (m_head->bufferSize/sizeof(T)));
+}
+
+template <typename T>
+T* FMManager<T>::Add()
+{
+    void* result = m_head->head;
+
+    assert(m_head->numOfFreeBlocks >= 0);
+
+    if(m_head->numOfFreeBlocks == 0)
+    {
+        /**< There are no empty blocks. Allocate the next available space and return */
+        result = m_head->head;
+        m_head->head = (void*)((char*)m_head->head + sizeof(T));
+    }
+    else
+    {
+        /**< There are empty blocks. So allocate at head and update head to next block. */
+        Bookmark* curBlock = ((Bookmark*)(m_head->head));
+        m_head->head = curBlock->nextEmptyBlock;
+        m_head->numOfFreeBlocks--;
+    }
+
+    m_head->numOfAllocatedBlocks++;
+    assert(result != NULL);
+    return new(result) T();
+}
+
+template <typename T>
+void FMManager<T>::Remove(T* obj)
+{
+    assert(obj != NULL);
+    assert(m_head->numOfAllocatedBlocks >= 0);
+
+    Bookmark* bkmk = (Bookmark*)((char*)obj + sizeof(Bookmark));
+    bkmk->nextEmptyBlock = m_head->head ;
+    m_head->head = (void*)bkmk;
+
+    m_head->numOfAllocatedBlocks--;
+    m_head->numOfFreeBlocks++;
 }
--- main.cpp
+++ main.cpp
@@ -7,16 +7,41 @@
 #define NUM_ALLOC 2000
 #define DEFAULT_SIZE 1024
 
+#define RUN_FMM_TEST 1
+#define RUN_NEW_TEST 1
+#define DEBUG 0
+
 using namespace std;
 
-int g_BufferSize;
-int g_NumOfAlloc;
+unsigned g_BufferSize;
+unsigned g_NumOfAlloc;
 clock_t g_StartTime,g_EndTime;
 
 class testData
 {
 public:
     int id;
+    int random1;
+    int random2;
+
+    testData()
+    {
+        this->id = 0;
+        this->random1 = 0;
+        this->random2 = 0;
+    }
+
+    ~testData()
+    {
+
+    }
+
+    void setData(int id)
+    {
+        this->id = id;
+        this->random1 = id << 14;
+        this->random2 = id >> 4;
+    }
 };
 
 int main(int argc, char* argv[])
@@ -24,8 +49,6 @@ int main(int argc, char* argv[])
     cout << "usage : FMManager -s <bufferSize> -n <numOfAllocations>\n\n" ;
     g_BufferSize = DEFAULT_SIZE;
     g_NumOfAlloc = NUM_ALLOC;
-
-    g_StartTime = clock();
 
     cout << "dbg :: Starting FMM with \n\tBufferSize : ";
     for(int i=1;i<argc;i++)
@@ -48,11 +71,80 @@ int main(int argc, char* argv[])
     assert(td_FFM->GetBufferStart() != NULL);
     cout << "Done!\n";
 
+    cout << "dbg :: Size of the testData block :: " << sizeof(testData);
+    cout << "\ndbg :: Buffer Capacity :: " << td_FFM->Capacity();
+    cout << "\ndbg :: Number of objects currently in buffer :: " << td_FFM->Count();
+    cout << "\ndbg :: Is buffer empty :: " << (td_FFM->IsEmpty() ? "Yes" : "No");
+    cout << "\ndbg :: Is buffer full :: " << (td_FFM->IsFull() ? "Yes" : "No");
+    cout << "\n\n";
+
+    testData * s_Objs[g_NumOfAlloc];
+    double elapsedTime;
+
+#if RUN_NEW_TEST
+    g_StartTime = clock();
+
+    for(unsigned i=0;i < g_NumOfAlloc;i++)
+    {
+        s_Objs[i] = new testData();
+        s_Objs[i]->setData(i);
+        cout << "Element "<< s_Objs[i]->id<< " has been added\n";
+    }
+
+    #if DEBUG
+    for(unsigned i=0;i < g_NumOfAlloc;i++)
+    {
+        cout << "obj "<<i<< "--id "<<s_Objs[i]->id << " random1 "<<s_Objs[i]->random1 << " random2 "<<s_Objs[i]->random2<<endl;
+    }
+    #endif // DEBUG
+
+    for(unsigned i=0;i < g_NumOfAlloc; i++)
+    {
+        delete s_Objs[i];
+    }
+
     g_EndTime = clock();
 
-    double elapsedTime = (double)(g_EndTime - g_StartTime) / CLOCKS_PER_SEC;
+    elapsedTime= (double)(g_EndTime - g_StartTime) / CLOCKS_PER_SEC;
 
-	cout << "\nstat :: Time taken for test to complete: " << elapsedTime << " seconds\n";
+	cout << "\nstat :: Time taken for default new allocations to complete: " << elapsedTime << " seconds\n";
+#endif // RUN_NEW_TEST
+
+#if RUN_FMM_TEST
+	g_StartTime = clock();
+
+    cout << "Capacity : " << td_FFM->Capacity() << " #ofAlloc : " << g_NumOfAlloc << endl;
+	assert(td_FFM->Capacity() >= g_NumOfAlloc);
+
+    cout << "Adding elements...";
+    for(unsigned i=0;i < g_NumOfAlloc;i++)
+    {
+        s_Objs[i] = td_FFM->Add();
+        s_Objs[i]->setData(i);
+        cout << "Element "<< s_Objs[i]->id<< " has been added\n";
+    }
+    cout << "Done.\n";
+
+    #if DEBUG
+    for(unsigned i=0;i < g_NumOfAlloc;i++)
+    {
+        cout << "obj "<<i<< "--id "<<s_Objs[i]->id << " random1 "<<s_Objs[i]->random1 << " random2 "<<s_Objs[i]->random2<<endl;
+    }
+    #endif // DEBUG
+
+    cout << "\nRemoving elements...";
+    for(unsigned i=0;i < g_NumOfAlloc; i++)
+    {
+        td_FFM->Remove(s_Objs[i]);
+    }
+    cout << "Done.";
+
+    g_EndTime = clock();
+
+    elapsedTime = (double)(g_EndTime - g_StartTime) / CLOCKS_PER_SEC;
+
+	cout << "\nstat :: Time taken for FMManager allocations to complete: " << elapsedTime << " seconds\n";
+#endif // RUN_FMM_TEST
 
     return 0;
 }
